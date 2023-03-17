e426b92aa36ddc4c2881a27ed5bebbd17cf7cc78
danilamdev <dalmabarri@gmail.com>
2023-03-17 13:55:02 -0300

    add animation success button

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,7 +1,8 @@
 import '../styles/contact.css'
-import {useMemo, useState} from 'preact/hooks'
+import {useEffect, useMemo, useState} from 'preact/hooks'
 
 import { sendMail } from '../services/fetchEmailApi'
+import {gsap} from 'gsap'
 
 function Contact(){
   const [name, setName] = useState('')
@@ -9,6 +10,7 @@ function Contact(){
   const [message, setMessage] = useState('')
   const [loading, setLoading] = useState(false)
   const [isSuccess, setIsSuccess] = useState(false)
+
 
   let hasNameError = useMemo(() => {
     if(name === '') return false
@@ -27,10 +29,10 @@ function Contact(){
       const response = await sendMail({name, email, message})
   
       if(response.status === 'sent'){
-        setIsSuccess(true)
         setName('')
         setEmail('')
         setMessage('')
+        setIsSuccess(true)
         
       }  
     } catch (error) {
@@ -41,11 +43,28 @@ function Contact(){
     
     setTimeout(() => {
       setIsSuccess(false)
-    }, 2000);
+    }, 5000);
     console.log({name, email, message})
   }
 
+  useEffect(()=> {
+   
+    if(!isSuccess) return
+    console.log('use Effect')
+    const anim = gsap.to("#success-msg", {
+      opacity: 1,
+      scale: 1,
+      ease: "elastic.out(1, 0.4)",
+      repeat:1,
+      repeatDelay:1.5,
+      yoyo: true,
+      duration: 1.2
+    });
+    
+    return () => anim.kill('#success-msg')
+  },[isSuccess])
 
+  console.log('success', isSuccess)
   return (
     <section id="contact" class="bg-white py-24 xl:py-36">
       <h2
@@ -76,21 +95,22 @@ function Contact(){
               <textarea resi aria-required required value={message} disabled={loading} name="mensaje" id="mensaje" cols="30" rows="10" placeholder='escribe tu mensaje...' onInput={(e) => setMessage(e.target.value)}  className='bg-slate-50 border border-slate-200 rounded px-2 py-4 focus:outline-indigo-400/40 resize-none  disabled:text-gray-400 placeholder:text-gray-300'></textarea>
             </div>
 
-            <button disabled={btnDisable} className='bg-cyan-400 text-lg text-white w-full xl:w-40 mx-auto rounded-full py-4 h-14 disabled:bg-slate-400 disabled:active:scale-100 hover:bg-cyan-500 active:scale-95 flex items-center justify-center gap-10' type='submit'>
+            <button disabled={btnDisable} className='bg-cyan-400 text-lg text-white w-full xl:w-40 mx-auto rounded-full py-4 h-14 disabled:bg-slate-300 disabled:active:scale-100 hover:bg-cyan-500 active:scale-95 flex items-center justify-center gap-10' type='submit'>
               
              {loading && <div className="spinner w-5 h-5"></div>}   
              
               <span className={`text-lg xl:${loading ? 'hidden' : 'block'}`}>{loading ? 'Enviando...' : 'enviar'}</span>
             </button>
 
-            <p className='text-center block p-12'>o comunicate por 
+            <p  className='text-center block p-12'>o comunicate por 
               <a className='text-green-500' href='https://wa.me/+5491167231003' target='_blank'> whatapp</a>
             </p>
           </form>
       </div>
 
-      {isSuccess && <p className='fixed bg-green-100 text-green-600 p-2 px-6 rounded text-lg bottom-40 z-50 left-2/4 -translate-x-20 '><strong className='text-2xl'>✓</strong> email enviado!</p>}
-      
+
+      {isSuccess && <p id='success-msg' className='fixed bg-slate-700 text-white px-8 py-6 rounded-lg text-md bottom-40 z-50 left-2/4 -translate-x-20 flex items-center gap-4 font-poppins scale-0 opacity-0'><strong className='text-2xl text-green-400 border-2 border-green-400 px-2 rounded-full'>✓</strong> email enviado!</p>}
+
     </section>
   )
 }
