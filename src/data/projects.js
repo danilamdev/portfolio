f86a6ca28983b6eefa5934a1d8aa3f7ebed92cbf
danilamdev <dalmabarri@gmail.com>
2023-08-16 16:49:57 -0300

    add projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,5 +1,37 @@
 export default [
   {
+    image:'/contactos.avif', 
+    title: 'App de Contactos',
+    tags: ['React', 'typescript', 'hooks'],
+    description: 'Aplicación de contactos con persistencia, favoritos y filtros',
+    githubLink:'https://github.com/danilamdev/React-Contacts-App',
+    siteUrl: 'https://react-contacts-app-omega.vercel.app/'
+  },
+  {
+    image:'/translate.avif', 
+    title: 'React translate',
+    tags: ['React', 'typescript'],
+    description: 'Aplicación de traducción de idiomas basado en google translate',
+    githubLink:'https://github.com/danilamdev/react-translate-now',
+    siteUrl: 'https://react-translate-now.vercel.app/'
+  },
+  {
+    image:'/cursos.avif', 
+    title: 'SitioWeb Cursos de extensión',
+    tags: ['Astro', 'react'],
+    description: 'Sitio web diseñado y desarrollado de información de cursos de extensión universitarios',
+    githubLink:'https://github.com/danilamdev/cursos-website',
+    siteUrl: 'https://www.cursosextensionuniversitarios.com/'
+  },
+  {
+    image:'/qr.webp', 
+    title: 'QR-generator',
+    tags: ['Astro', 'tailwind', 'cloudflare'],
+    description: 'Generador de QR en base a una url que uno quiera, utilizando las serverless functions de cloudflare',
+    githubLink:'https://github.com/danilamdev/qr-generator',
+    siteUrl: 'https://qrgenerator-9yd.pages.dev/'
+  },
+  {
     image:'/list.webp', 
     title: 'Supermarket List',
     tags: ['React', 'framer motion', 'tailwind'],
@@ -15,14 +47,7 @@ export default [
     githubLink:'https://github.com/danilamdev/weapi',
     siteUrl: 'https://weapi.vercel.app/'
   },
-  {
-    image:'/qr.webp', 
-    title: 'QR-generator',
-    tags: ['Astro', 'tailwind', 'cloudflare'],
-    description: 'Generador de QR en base a una url que uno quiera, utilizando las serverless functions de cloudflare',
-    githubLink:'https://github.com/danilamdev/qr-generator',
-    siteUrl: 'https://qrgenerator-9yd.pages.dev/'
-  },
+
   {
     image:'/coderhouse.webp', 
     title: 'Curso Backend - Ecommerce',
@@ -30,13 +55,5 @@ export default [
     description: 'Projecto final de programación Backend de Coderhouse. React en el front y nodejs, Express, mysql, mondodb y firabase en el Backend.',
     githubLink:'https://github.com/danilamdev/proyecto-final-backend',
     siteUrl: 'unavailable'
-  },
-  {
-    image:'/translate.avif', 
-    title: 'React translate',
-    tags: ['React', 'typescript'],
-    description: 'Aplicación de traducción de idiomas basado en google translate',
-    githubLink:'https://github.com/danilamdev/react-translate-now',
-    siteUrl: 'https://react-translate-now.vercel.app/'
   }
 ]
